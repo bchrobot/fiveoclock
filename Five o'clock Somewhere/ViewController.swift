4c146306311a4d4349e9010780ef7fce2862cc1a
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2017-04-08 11:25:44 -0400

    Initial Commit

---
+++ Five o'clock Somewhere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Five o'clock Somewhere
+//
+//  Created by Benjamin Chrobot on 4/8/17.
+//  Copyright © 2017 Bytesized Bison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
